040d671fc9a2b142baffb7e560c398d39ea02291
hasnatfahmidkhan <hasnatfahmidkhan@gmail.com>
2025-07-20 16:20:53 +0600

    add all the files from day 1 to till now

---
+++ Emran Sir/JavaScript/script.js
@@ -0,0 +1,14 @@
+// console.log("Hello Console!");
+// document.getElementById("demo").innerHTML = "Nothing";
+// let x = document.getElementById("demo").innerHTML;
+// alert(x); // get full content of the id and with the tag
+// let y = document.getElementById("demo").innerText;
+// alert(y) // get full content of the id not include inside tag
+// var x = 6;
+// var result = x % 2 == 0 ? "Even" : "Odd";
+// console.log(x, result)
+
+var x = 1;
+var y = 1;
+console.log(x++);
+console.log(++y)
